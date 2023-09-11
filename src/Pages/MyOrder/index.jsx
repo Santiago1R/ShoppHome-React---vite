012838b79501b077bc9cfb7e4a299c1561ce12aa
Santiago Rios <riosnelson2017@gmail.com>
2023-09-11 17:58:12 -0500

    Página de MyOrders: lista de órdenes // 23

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,13 +1,20 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShoppingCardContext } from "../../Context";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import Layout from "../../Components/Layout";
 import OrderCard from "../../Components/OrderCard";
 const MyOrder = () => {
   const context = useContext(ShoppingCardContext);
-  console.log(context.order?.slice(-1)[0])
+  
   return (
     <Layout>
-      My Order
+      <div className="flex items-center justify-center w-80 relative mb-6">
+        <Link to="/my-orders" className="absolute left-0">
+          <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
+        </Link>
+        <h1>My Orders</h1>
+      </div>
       <div className="flex flex-col w-80 gap-4">
         {context.order?.slice(-1)[0].products.map((product) => (
           <OrderCard
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,26 @@
-import Layout from "../../Components/Layout"
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import Layout from "../../Components/Layout";
+import OrdersCard from "../../Components/OrdersCard";
+import { ShoppingCardContext } from "../../Context";
 const MyOrders = () => {
-    return (
-      <Layout>
-        My Orders
-      </Layout>
-    )
-  }
-  
-  export default MyOrders
-  
+  const context = useContext(ShoppingCardContext);
+  return (
+    <Layout>
+      <div className="flex items-center justify-center w-80 relative">
+        <h1>My Orders</h1>
+      </div>
+      {context.order.map((order, index) => {
+        <Link key={index} to={`/my-orders/${order.id}`}>
+          <OrdersCard
+            totalPrice={order.totalPrice}
+            totalProducts={order.totalProducts}
+          />
+          ;
+        </Link>;
+      })}
+    </Layout>
+  );
+};
+
+export default MyOrders;
