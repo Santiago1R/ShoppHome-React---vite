5a0e7a271e74ba67cfb7dada56cf1f3bc4cf1aa0
Santiago Rios <riosnelson2017@gmail.com>
2023-09-13 12:20:16 -0500

    Se agrega página de MyAccount mostrando datos y editándolos

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     if (hasUserAnAccount && !isUserSignOut) {
       return (
         <>
-          <li className="text-black/60">Santi@gmail.com</li>
+          <li className="text-black/60">{parsedAccount?.email}</li>
           <li>
             <NavLink
               to="/my-orders"
